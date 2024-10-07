79748096830125cedec281601d938364136e7729
STUDENT Bartosz Wicha <Bartosz.Wicha@students.ittralee.ie>
2024-10-07 16:51:56 +0100

    adding files

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,10 +1,10 @@
 /* GET 'home' page */
 const Welcome = function(req, res){
-res.render('index', { title: 'Home' });
+res.render('welcome', { title: 'Home' });
 };
 /* GET 'Location info' page */
 const GameList = function(req, res){
-res.render('index', { title: 'Games List' });
+res.render('gameList', { title: 'Games List' });
 };
 
 module.exports = {

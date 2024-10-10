95b3ed641c86284af247c8663c711c7866e2688b
BartoszWicha <bartosz.wicha@students.ittralee.ie>
2024-10-10 17:32:12 +0100

    added loggin and register UI

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,13 +1,46 @@
 /* GET 'home' page */
 const Welcome = function(req, res){
-res.render('welcome', { title: 'Home' });
+res.render('welcome', { 
+    title: 'Welcome',
+});
 };
 /* GET 'Location info' page */
 const GameList = function(req, res){
-res.render('gameList', { title: 'Games List' });
+res.render('gameList', { 
+    title: 'Games List',
+    pageHeader: { title: "isTitle"},
+    Reviews: [
+        {
+        Game:"Throne and Liberty",
+        Score: "75",
+        Cost: "20$",
+        Genres: ["Action", "Adventure", "RPG", "Multiplayer"]
+        },
+        {
+        Game:"Diablo 4",
+        Score: "60",
+        Cost: "30$",
+        Genres: ["Action", "Adventure", "RPG", "Multiplayer"],
+        } 
+    ]
+    });
+};
+
+const Register = function(req, res){
+    res.render('Register', { 
+        title: 'register',
+});
+};
+
+const Login = function(req, res){
+    res.render('Login', { 
+        title: 'login',
+});
 };
 
 module.exports = {
     Welcome,
     GameList,
+    Login,
+    Register
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ctrlLocations = require('../controllers/locations.js');
-const ctrlOthers = require('../controllers/others')
+const ctrlOthers = require('../controllers/others');
 /* Locations pages */
 router.get('/', ctrlLocations.Welcome);
 router.get('/GameList', ctrlLocations.GameList);
+router.get('/Register', ctrlLocations.Register);
+router.get('/Login', ctrlLocations.Login);
+
 /* Other pages */
 router.get('/about', ctrlOthers.about);
 module.exports = router;

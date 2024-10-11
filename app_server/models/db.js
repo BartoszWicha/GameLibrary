184961e1d956f9c33e12de57dae45e10320262d6
BartoszWicha <bartosz.wicha@students.ittralee.ie>
2024-10-11 11:10:03 +0100

    added database schemas

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,7 @@
-/*const mongoose = require('mongoose');
-const dbURI =
-"mongodb+srv://username:password@cluster0.00wzfxg.mongodb.net/Loc8r?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
+const dbURI = "mongodb+srv://wichabartosz:6jaeP!v36i9u@cluster0.in0dz.mongodb.net/GameLibrary?retryWrites=true&w=majority";
+
+
 try {
 mongoose.connect(
 dbURI,
@@ -12,4 +13,40 @@ err=> {console.log(err)}
 catch (e) {
 console.log("could not connect");
 }
-//require('./GamesList');*/
+require('./GameList');
+
+const GameListSchema = new mongoose.schema({
+    Game: String ,
+    Score: {
+        type:Number,
+        'default': null,
+        min: 0,
+        max: 100},
+    Cost: {
+        type: Number,
+        'default': 0,},
+    Genres: [String],
+    Review: [reviewSchema]
+})
+
+mongoose.model('GameReviews', GameListSchema);
+
+const reviewSchema = new mongoose.schema({
+    Author: {
+        type: String,
+        default: 'Anonymouse'
+    },
+    Score: {
+        type:Number,
+        required: true,
+        min:0,
+        max: 100},
+    Timestamp: {
+        type: Date,
+        'default': Date.now
+    },
+    ReviewText: {
+        type: String,
+        'default': "No review description was added by user" 
+    }
+})
